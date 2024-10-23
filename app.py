8f5d928e40c683597dde9f4b483e5e299007f13d
MiotyR <mramaha96@gmail.com>
2024-10-23 16:19:46 +0200

    user input to display exercise specs

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ import streamlit as st
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-ANSWER_STR = """
-SELECT * FROM beverages
-CROSS JOIN food_items
-"""
-
-#solution_df = duckdb.sql(ANSWER_STR).df()
+# ANSWER_STR = """
+# SELECT * FROM beverages
+# CROSS JOIN food_items
+# """
+#
+# solution_df = duckdb.sql(ANSWER_STR).df()
 
 with st.sidebar:
     theme = st.selectbox(
@@ -27,31 +27,33 @@ with st.sidebar:
 st.header("Enter your code:")
 query = st.text_area(label="Your SQL code here", key="user_input")
 
-# if query:
-#     result = duckdb.sql(query).df()
-#     st.dataframe(result)
-#
-#     try:
-#         result = result[solution_df.columns]
-#         st.dataframe(result.compare(solution_df))
-#     except KeyError as e:
-#         st.write("Some columns are missing")
-#
-#     n_lines_difference = result.shape[0] - solution_df.shape[0]
-#     if n_lines_difference != 0:
-#         st.write(
-#             f"result has a {n_lines_difference} lines difference with the solution_df"
-#         )
-#
-# tab2, tab3 = st.tabs(["Tables", "Solution"])
-#
-# with tab2:
-#     st.write("table: beverages")
-#     st.dataframe(beverages)
-#     st.write("table: food_items")
-#     st.dataframe(food_items)
-#     st.write("expected:")
-#     st.dataframe(solution_df)
-#
-# with tab3:
-#     st.write(ANSWER_STR)
+if query:
+    result = con.execute(query).df()
+    st.dataframe(result)
+
+    # try:
+    #     result = result[solution_df.columns]
+    #     st.dataframe(result.compare(solution_df))
+    # except KeyError as e:
+    #     st.write("Some columns are missing")
+    #
+    # n_lines_difference = result.shape[0] - solution_df.shape[0]
+    # if n_lines_difference != 0:
+    #     st.write(
+    #         f"result has a {n_lines_difference} lines difference with the solution_df"
+    #     )
+
+    tab2, tab3 = st.tabs(["Tables", "Solution"])
+
+    with tab2:
+        exercise_tables = exercise.loc[0, "tables"]
+        for table in exercise_tables:
+            st.write(f'table: {table}')
+            df_table = con.execute(f"SELECT * FROM {table}").df()
+            st.dataframe(df_table)
+
+    with tab3:
+        exercise_name = exercise.loc[0, "exercise_name"]
+        with open(f"answers/{exercise_name}.sql", "r") as f:
+            answer = f.read()
+        st.write(answer)
